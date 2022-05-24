d869bcf145c19445b41ede0b55c53f93494aba0f
Shaurya Aher <shauryaaher1200@gmail.com>
2022-05-24 16:00:11 +0530

    Check the HTTP method of the client

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "My name" });
+  if (req.method !== "GET") {
+    res.status(405).send(`HTTPS ${405} Method Not Allowed`);
+  } else {
+    res.status(200).json({ name: "My name" });
+  }
 }
--- pages/api/serverless.js
+++ pages/api/serverless.js
@@ -1,9 +1,13 @@
 export default function serverless(req, res) {
-	res.send(`
+  if (req.method !== "GET") {
+    res.status(405).send(`HTTPS ${405} Method Not Allowed`);
+  } else {
+    res.status(200).send(`
 		<html>
 		<body bgcolor="black">
 			<h1 style="color: white">I love serverless functions.</h1>
 		</body>
 		</html>
 	`);
+  }
 }
