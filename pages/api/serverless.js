fef777ff2cf1f0f00c6105874e2857b5af15c6fa
Shaurya Aher <shauryaaher1200@gmail.com>
2022-05-25 16:00:19 +0530

    Change the  API route data

--- pages/api/serverless.js
+++ pages/api/serverless.js
@@ -6,7 +6,7 @@ export default function serverless(req, res) {
     res.status(200).send(`
 		<html>
 		<body bgcolor="black">
-			<h1 style="color: white">I love serverless functions.</h1>
+			<h1 style="color: white">The word 'serverless'(in serverless functions) is a misnomer. It actually means that you don't have to manage or scale any servers on your own. You can just deploy you code and the cloud service handles the rest of the work for you.</h1>
 		</body>
 		</html>
 	`);
