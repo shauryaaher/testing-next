60256add8ddc983870e92d5d84ccf79fa0aba4f0
Shaurya Aher <72655598+shauryaaher@users.noreply.github.com>
2022-05-26 17:04:47 +0000

    Add head tag to each pageand edit homepage

--- pages/file.jsx
+++ pages/file.jsx
@@ -1,11 +1,19 @@
 import { Component, Fragment } from "react";
 import styles from "../styles/File.module.css";
 import Link from "next/link";
+import Head from "next/head";
 
 class File extends Component {
   render() {
     return (
       <Fragment>
+        <Head>
+          <title>Link to a dynamic routing page</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <h1 className={styles.container}>Hello World!</h1>
         <Link href="/dyn/people">
           <a className={styles.link}>Dynamic routing</a>
--- pages/reactive.jsx
+++ pages/reactive.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
+import Head from "next/head";
 
 function Reactive() {
   const [reader, writer] = useState(0);
   return (
     <>
+      <Head>
+        <title>Counter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <h2>{reader}</h2>
-      <button onClick={() => {writer(reader + 1)}}>Increment</button>
-      <button onClick={() => {writer(reader - 1)}}>Decrement</button>
+      <button
+        onClick={() => {
+          writer(reader + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          writer(reader - 1);
+        }}
+      >
+        Decrement
+      </button>
     </>
   );
 }
