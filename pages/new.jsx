69fe574a9304b7b6b728146201dedc12e757ab4a
Shaurya Aher <72655598+shauryaaher@users.noreply.github.com>
2022-05-28 15:29:50 +0000

    Remove unnecessary console.log()

--- pages/new.jsx
+++ pages/new.jsx
@@ -3,9 +3,8 @@ import Head from "next/head";
 import Link from "next/link";
 
 export async function getStaticProps() {
-  const res = await fetch("https://testing-next-silk.vercel.app/api/new");
+  const res = await fetch("https://testing-next-silk.vercel.app/api/new/");
   const data = await res.json();
-  console.log(data);
   return {
     props: {
       users: data,
@@ -20,9 +19,6 @@ export default function NewRoute({ users }) {
         <title>New</title>
       </Head>
       <div className={styles.main}>
-        <Link href="/">
-          <a>Go to homepage</a>
-        </Link>
         {users.map((user) => {
           return (
             <ul key={user.id}>
@@ -30,6 +26,9 @@ export default function NewRoute({ users }) {
             </ul>
           );
         })}
+        <Link href="/">
+          <a>Go to homepage</a>
+        </Link>
       </div>
     </>
   );
