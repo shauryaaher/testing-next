b3fb419ff13cd6db2923558bacc587a0ae5a7504
Shaurya Aher <shauryaaher1200@gmail.com>
2022-05-28 13:10:50 +0530

    Change function responses

--- pages/middleware/_middleware.js
+++ pages/middleware/_middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
 export function middleware(request) {
-    const country = request.geo.country;
-    return new Response(`Your country is ${country}`);
+    const country = request.geo.country || "unavailible";
+    return new Response(`Your country is ${country}.`);
 }
