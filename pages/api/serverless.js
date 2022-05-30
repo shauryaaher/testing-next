60e5a265163918b52a5bd4d0d63bac7623991d96
Shaurya Aher <shauryaaher1200@gmail.com>
2022-05-30 18:12:24 +0530

    Add a hyperlink to the homepage in the /serverless route

--- pages/api/serverless.js
+++ pages/api/serverless.js
@@ -11,10 +11,15 @@ export default function serverless(req, res) {
 					color: white;
 					font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
 				}
+				a {
+					color: dodgerblue;
+					font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+				}
 			</style>
 		</head>
 		<body bgcolor="black">
 			<h1 class="text">The word 'serverless'(in serverless functions) is a misnomer. It actually means that you don't have to manage or scale any servers on your own. You can just deploy you code and the cloud service handles the rest of the work for you.</h1>
+			<a href="https://testing-next-silk.vercel.app/">Go to homepage</a>
 		</body>
 		</html>
 	`);
